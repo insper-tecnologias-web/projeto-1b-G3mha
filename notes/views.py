45ed45d4a0be343175ec1de825ce397e3fc08533
Enricco Gemha <riccogemha@gmail.com>
2022-09-20 14:40:28 -0300

    updating crud

--- notes/views.py
+++ notes/views.py
@@ -7,7 +7,15 @@ def index(request):
         title = request.POST.get('titulo')
         content = request.POST.get('detalhes')
         note = Note.objects.create(title=title, content=content)
-        note.save()
+        if request.POST.__contains__('create'):
+            note.save()
+        
+        if request.POST.__contains__('id'):
+            note_delete_id = request.POST.get('id')
+            note = Note(id=note_delete_id, title=title, content=content)
+            if request.POST.__contains__('delete'):
+                note.delete()
+                
         return redirect('index')
     else:
         all_notes = Note.objects.all()
